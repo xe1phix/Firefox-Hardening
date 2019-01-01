f549d39e7ffbabf9c05692fe221fb17294e4f66a
Xe1phix <xe1phix@gmail.com>
2019-01-01 04:35:53 +0000

    Upload New File

---
+++ Xe1phix-TorMessenger/CHEF-KOCH-Tor-Messenger-user.js
@@ -0,0 +1,64 @@
+# Mozilla User Preferences
+
+/* This is an hardened version prefs.js file by CHEF-KOCH
+ *
+ * The project can be found here: https://github.com/CHEF-KOCH/Tor-Messenger-user.js
+ * 
+ * This is seperated from the official Tor Messenger Project.
+ */
+
+user_pref("app.update.auto", true);
+user_pref("app.update.enabled", true);
+user_pref("beacon.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.update", true);
+user_pref("canvas.filters.enabled", true);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.broadcastChannel.enabled", false);
+user_pref("dom.enable_performance", false);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("extensions.blocklist.pingCountVersion", 0);
+user_pref("extensions.pendingOperations", false);
+user_pref("extensions.shownSelectionUI", true);
+user_pref("extensions.torlauncher.prompt_at_startup", false);
+user_pref("extensions.ui.dictionary.hidden", true);
+user_pref("extensions.ui.locale.hidden", true);
+user_pref("extensions.update.autoUpdateDefault", true);
+user_pref("extensions.update.enabled", true);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("network.cookie.cookieBehavior", 2);
+user_pref("network.cookie.lifetime.days", 2);
+user_pref("network.cookie.prefsMigrated", true);
+user_pref("network.dns.disableIPv6", true);
+user_pref("network.dnsCacheEntries", 200);
+user_pref("network.http.spdy.enabled.deps", false);
+user_pref("network.http.spdy.enabled.http2", false);
+user_pref("network.http.spdy.enabled.v3-1", false);
+user_pref("network.http.spdy.allow-push", false);
+user_pref("network.http.speculative-parallel-limit", 2);
+user_pref("network.predictor.cleaned-up", true);
+user_pref("noscript.forbidFlash", true);
+user_pref("noscript.forbidFonts", true);
+user_pref("noscript.forbidJava", true);
+user_pref("noscript.forbidSilverlight", true);
+user_pref("social.whitelist", "");
+user_pref("browser.chrome.toolbar_tips", false);
+user_pref("browser.formfill.saveHttpsForms", false);
+user_pref("browser.formfill.expire_days", 7);
+user_pref("device.sensors.enabled", false);
+user_pref("devtools.remote.wifi.scan", false);
+user_pref("network.http.pipelining", true);
+user_pref("network.http.pipelining.max-optimistic-requests", 2);
+user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("network.http.referer.spoofSource", true);
+user_pref("plugin.scan.Acrobat; "");
+user_pref("plugin.scan.WindowsMediaPlayer; "");
+user_pref("plugins.click_to_play", true);
+// logging
+// https://github.com/arlolra/ctypes-otr/issues/49
+// https://trac.torproject.org/projects/tor/ticket/17411#comment:5
+//user_pref("purple.logging.log_chats", false);
+//user_pref("purple.logging.log_ims", false);
+//user_pref("purple.logging.log_system", false);
+// end logging
